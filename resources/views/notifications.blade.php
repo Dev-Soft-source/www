19d868d0234830cd07e62b4a61eda26d4ce26282
sviridov-dev <arkagisv@gmail.com>
2026-03-12 16:57:56 +1000

    # fix notification list date lang

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -202,7 +202,7 @@
                                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                             </svg>
-                                                            {{ \Carbon\Carbon::parse($notification->added_on)->format('M d, Y') }} at {{ \Carbon\Carbon::parse($notification->added_on)->format('h:i A') }}
+                                                            {{ \Carbon\Carbon::parse($notification->added_on)->locale(app()->getLocale())->translatedFormat('M d, Y h:i A') }}
                                                         </p>
                                                     </div>
                                                 </div>
