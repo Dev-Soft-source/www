62b7c9ed95acf19f521ff2c0f27570de0533ce32
sviridov-dev <arkagisv@gmail.com>
2026-03-24 05:24:51 +1000

    #fix

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -50,49 +50,12 @@ class ChatsController extends Controller
 
     public function chatDetail($lang = null, $id, $passenger)
     {
-        $languages = Language::all();
-        // Store the selected language in the session
-        if ($lang && in_array($lang, $languages->pluck('abbreviation')->toArray())) {
-            session(['selectedLanguage' => $lang]);
-        }
-        $chatsPage = null;
-        $selectedLanguage = session('selectedLanguage');
-        if ($selectedLanguage) {
-            // Find the language by abbreviation
-            $selectedLanguage = Language::where('abbreviation', $selectedLanguage)->first();
-            $chatsPage = ChatsPageSettingDetail::where('language_id', $selectedLanguage->id)->first();
-        } else {
-            $selectedLanguage = Language::where('is_default', 1)->first();
-            $chatsPage = ChatsPageSettingDetail::where('language_id', $selectedLanguage->id)->first();
-        }
+            
+        $chatsPage = ChatsPageSettingDetail::getByLanguageWithFallback(
+            $this->selectedLanguage->id,
+            $this->defaultLang->id
+        );
 
-        $notifications = null;
-        if (auth()->user()) {
-            $user_id = auth()->user()->id;
-            $notifications = Notification::where('is_delete', '0')->where(function ($query) use ($user_id) {
-                // Ratings where type is 1 and ride_id belongs to the user
-                $query->where('type', '1')
-                    ->whereHas('ride', function ($query) use ($user_id) {
-                        $query->where('added_by', $user_id);
-                    });
-            })
-                ->orWhere(function ($query) use ($user_id) {
-                    // Ratings where type is 2 and booking_id belongs to the user
-                    $query->where('type', '2')
-                        ->whereHas('booking', function ($query) use ($user_id) {
-                            $query->where('user_id', $user_id);
-                        });
-                })
-                ->orWhere(function ($query) use ($user_id) {
-                    // Ratings where type is null and receiver_id belongs to the user
-                    $query->where('type', null)
-                        ->whereHas('receiver', function ($query) use ($user_id) {
-                            $query->where('id', $user_id);
-                        });
-                })
-                ->orderBy('id', 'desc')
-                ->get();
-        }
 
         $ride = Ride::whereId($id)->first();
         $passenger = User::whereId($passenger)->first();
@@ -103,7 +66,7 @@ class ChatsController extends Controller
                 ->with('error', 'Ride or passenger not found.');
         }
         
-        return view('chat_detail', ['languages' => $languages, 'selectedLanguage' => $selectedLanguage, 'notifications' => $notifications, 'ride' => $ride, 'passenger' => $passenger, 'chatsPage' => $chatsPage]);
+        return view('chat_detail', ['ride' => $ride, 'passenger' => $passenger, 'chatsPage' => $chatsPage]);
     }
 
     public function fetchMessages($id, $userId)
