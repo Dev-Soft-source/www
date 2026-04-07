3a2ed555d89bac065660f6b310a7498b29188c66
sviridov-dev <arkagisv@gmail.com>
2026-04-07 16:56:24 +1000

    # sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($urls as $url)
     <url>
@@ -14,14 +14,14 @@
     @if (isset($getRide->defaultRideDetail[0]->departure) && isset($getRide->defaultRideDetail[0]->destination) && $getRide->defaultRideDetail[0]->departure != null)
     @php
       $date = Carbon\Carbon::parse($getRide->created_at);
-    @endphp 
+    @endphp
       <url>
         <loc>{{ route('ride_detail', ['lang' => 'en', 'departure' => $getRide->defaultRideDetail[0]->departure, 'destination' => $getRide->defaultRideDetail[0]->destination, 'id' => $getRide->id]) }}</loc>
         <lastmod>{{date("Ymd\THis\Z", strtotime($date))}}</lastmod>
         <priority>0.80</priority>
-      </url>    
+      </url>
     @endif
-    
+
     @endforeach
 @endif
 </urlset>
